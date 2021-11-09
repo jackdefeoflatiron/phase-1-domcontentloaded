2828508b12f7e4fe8a587559c8769dd2589b0533
John DeFeo <jackd3243@gmail.com>
2021-11-09 13:38:56 -0500

    commit tree 6768a0f29f887afeecd111d876d1c6d8c848e998

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-// Your code goes here
+document.addEventListener("DOMContentLoaded", () => func())
+
+   
+
+const func = () => document.getElementById('text').innerHTML = 'This is really cool!'
+
+// document.addEventListener( "DOMContentLoaded",  () => updateDOM());
+//   //select document" add event and type and what fucntion parm
+  
+//   let  updateDOM = () => document.getElementById( "text" ).innerHTML = "This is really cool!";
+  //set up the function and the placement
